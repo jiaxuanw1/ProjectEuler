338475cb4a3ef5f4eb6a71bb2d2f4f80a1545a83
Jiaxuan Wang <jiaxuanw1@gmail.com>
2020-03-26 15:17:40 -0400

    Solve problem 3

---
+++ java/Euler_003.java
@@ -0,0 +1,21 @@
+
+public class Euler_003 {
+
+	public static void main(String[] args) {
+		long n = 600851475143L;
+		int factor = 3;
+
+		while (factor < n) {
+			if (n % factor == 0) {
+				n /= factor;
+				factor = 3;
+			} else {
+				factor += 2;
+			}
+		}
+
+		System.out.println(n);
+
+	}
+
+}
