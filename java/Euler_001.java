854fd4badcc858eedb9477c6ce16731d89d54d7d
Jiaxuan Wang <jiaxuanw1@gmail.com>
2020-03-26 15:03:25 -0400

    Solve problem 1

---
+++ java/Euler_001.java
@@ -0,0 +1,15 @@
+
+public class Euler_001 {
+
+	public static void main(String[] args) {
+		int sum = 0;
+
+		for (int i = 3; i < 1000; i++) {
+			if (i % 3 == 0 || i % 5 == 0)
+				sum += i;
+		}
+
+		System.out.println(sum);
+	}
+
+}
