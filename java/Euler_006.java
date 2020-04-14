4c38c4816bd02f7cfceed754f8765b82232447c3
Jiaxuan Wang <jiaxuanw1@gmail.com>
2020-04-14 12:22:41 -0400

    P6: Optimize Java solution with sum formulas

--- java/Euler_006.java
+++ java/Euler_006.java
@@ -1,15 +1,18 @@
 
 public class Euler_006 {
 
+	public static int naturalSum(int n) {
+		return n * (n + 1) / 2;
+	}
+
+	public static int squareSum(int n) {
+		return n * (n + 1) * (n * 2 + 1) / 6;
+	}
+
 	public static void main(String[] arsg) {
-		int sumOfSquares = 0, sum = 0;
-
-		for (int i = 1; i <= 100; i++) {
-			sumOfSquares += i * i;
-			sum += i;
-		}
-
-		System.out.println(sum * sum - sumOfSquares);
+		int sumOfSquares = squareSum(100);
+		int squareOfSum = (int) Math.pow(naturalSum(100), 2);
+		System.out.println(squareOfSum - sumOfSquares);
 	}
 
 }
