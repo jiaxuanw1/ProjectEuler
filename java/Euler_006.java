8d7cff1bbc4836944a5550df6fdfaa4a55b39d80
Jiaxuan Wang <jiaxuanw1@gmail.com>
2020-04-14 12:25:47 -0400

    P6: Fixed typo

--- java/Euler_006.java
+++ java/Euler_006.java
@@ -9,7 +9,7 @@ public class Euler_006 {
 		return n * (n + 1) * (n * 2 + 1) / 6;
 	}
 
-	public static void main(String[] arsg) {
+	public static void main(String[] args) {
 		int sumOfSquares = squareSum(100);
 		int squareOfSum = (int) Math.pow(naturalSum(100), 2);
 		System.out.println(squareOfSum - sumOfSquares);
